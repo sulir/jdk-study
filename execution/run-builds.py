9035dd1aba597826b176bc5d664f2a12c51d9b08
Matúš Sulír <matus.sulir@gmail.com>
2025-03-07 01:24:39 +0100

    Refactor path operations using pathlib

--- execution/run-builds.py
+++ execution/run-builds.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 from csv import DictReader, DictWriter
 from logging import basicConfig, info
-from os import rename, scandir
-from os.path import abspath, basename, dirname, isfile, join
 from pathlib import Path
 from random import seed, shuffle
 from re import sub
 from subprocess import DEVNULL, check_output, run, STDOUT
 from sys import argv, path
 from zlib import crc32
-path.insert(1, dirname(dirname(abspath(__file__))))
+path.insert(1, str(Path(__file__).resolve().parent.parent))
 from common import IMAGE_NAME, LOG_CONFIG, MIN_JAVA, MAX_JAVA, RANDOM_SEED, TOOLS
 
 RESULTS_CSV = 'results.csv'
@@ -21,15 +19,15 @@ DOCKER_PROJECT_SRC = '/mnt/project'
 def run_builds(dataset_dir, results_dir):
     basicConfig(**LOG_CONFIG)
     remove_cache_volumes()
-    Path(results_dir).mkdir(parents=True, exist_ok=True)
-    Path(results_dir, RESULTS_CSV).touch()
+    results_dir.mkdir(parents=True, exist_ok=True)
+    (results_dir / RESULTS_CSV).touch()
 
-    project_dirs = sorted([file.path for file in scandir(dataset_dir) if file.is_dir()])
+    project_dirs = sorted([file for file in dataset_dir.iterdir() if file.is_dir()])
     seed(RANDOM_SEED)
     shuffle(project_dirs)
     project_dirs = remaining_projects(project_dirs, results_dir)
 
-    with open(join(results_dir, RESULTS_CSV), 'a') as out_file:
+    with open(results_dir / RESULTS_CSV, 'a') as out_file:
         versions_header = ['java%d' % v for v in range(MIN_JAVA, MAX_JAVA + 1)]
         # noinspection PyTypeChecker
         writer = DictWriter(out_file, ['name', 'commit', 'tool', 'wrapper'] + versions_header)
@@ -54,14 +52,14 @@ def get_volume_name(cache_dir):
     return '%s_%08x' % (name, crc32(name.encode()))
 
 def remaining_projects(project_dirs, results_dir):
-    with open(join(results_dir, RESULTS_CSV), 'r') as results_file:
+    with open(results_dir / RESULTS_CSV, 'r') as results_file:
         reader = DictReader(results_file)
         finished = {project['name'] for project in reader}
 
     return [p_dir for p_dir in project_dirs if get_project_name(p_dir) not in finished]
 
 def get_project_name(project_dir):
-    return basename(project_dir).replace('_', '/', 1)
+    return project_dir.name.replace('_', '/', 1)
 
 def build_project(project_name, project_dir, results_dir):
     info("Analyzing %s", project_name)
@@ -87,24 +85,24 @@ def analyze_project(project_name, project_dir):
     return builder, result
 
 def prepare_log_dir(project_dir, results_dir):
-    log_dir = Path(results_dir, basename(project_dir))
+    log_dir = results_dir / project_dir.name
     log_dir.mkdir(exist_ok=True)
     for file in log_dir.iterdir():
         if file.is_file():
             file.unlink()
-    return str(log_dir)
+    return log_dir
 
 def get_commit(project_dir):
     return check_output(['git', 'rev-parse', 'HEAD'], cwd=project_dir).decode().strip()
 
 def detect_tool(project_dir):
     for tool in TOOLS:
-        if any(isfile(join(project_dir, file)) for file in tool.files):
+        if any((project_dir / file).is_file() for file in tool.files):
             return tool
     raise FileNotFoundError(f"No build script found in {project_dir}")
 
 def detect_wrapper(project_dir, tool):
-    if isfile(join(project_dir, tool.wrapper)):
+    if (project_dir / tool.wrapper).is_file():
         return tool.wrapper
     else:
         return None
@@ -118,15 +116,15 @@ def build_project_with_java(project_dir, java_version, builder, log_dir):
                *volume_args,
                f'{IMAGE_NAME}:{java_version}', builder]
 
-    log_path = join(log_dir, '%02d' % java_version)
-    with open(log_path, 'w') as log_file:
+    log = log_dir / ('%02d' % java_version)
+    with open(log, 'w') as log_file:
         exitcode = run(command, stdin=DEVNULL, stdout=log_file, stderr=STDOUT, bufsize=0).returncode
 
-    rename(log_path, log_path + ('.pass' if exitcode == 0 else '.fail'))
+    log.rename(log.with_suffix('.pass' if exitcode == 0 else '.fail'))
     return exitcode
 
 if __name__ == '__main__':
     if len(argv) == 3:
-        run_builds(argv[1], argv[2])
+        run_builds(Path(argv[1]), Path(argv[2]))
     else:
-        print("Usage: %s <dataset_dir> <results_dir>" % basename(__file__))
+        print("Usage: %s <dataset_dir> <results_dir>" % Path(__file__).name)
